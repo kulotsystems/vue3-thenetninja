2a0911323edf378ef5b10d861bbc8442bded554f
Arvic S. Babol <kulotsystems@gmail.com>
2022-08-04 18:38:40 +0800

    Lesson 3: Vue.js Basics (part 2)

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'http://www.kulotsystems.tech',
             showBooks: true,
-            title    : 'The Final Empire',
-            author   : 'Brandon Sanderson',
-            age      : 45
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss' , img: 'assets/1.jpg', isFav: true  },
+                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true  },
+            ]
+        }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter(book => book.isFav);
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav;
         }
     }
 });
