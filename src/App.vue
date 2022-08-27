47893d3a57e80dbd786722377f373902c191fb3e
Arvic S. Babol <kulotsystems@gmail.com>
2022-08-27 10:17:48 +0800

    Lesson 5: The Vue CLI (replaced with Vite) & Bigger Projects (part 2)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
     <h1>{{ title }}</h1>
-    <Modal/>
+    <p>Welcome...</p>
+    <teleport to="#modals" v-if="showModal">
+        <Modal theme="sale" @close="toggleModal">
+            <h1>Ninja Giveaway!</h1>
+            <p>Grab your ninja swag for half price!</p>
+            <template v-slot:links>
+                <a href="#">Sign Up Now</a>
+                <a href="#">More Info</a>
+            </template>
+        </Modal>
+    </teleport>
+    <teleport to="#modals" v-if="showModal2">
+        <Modal theme="sale" @close="toggleModal2">
+            <h1>Sign Up to the News Letter</h1>
+            <p>For updates and promo codes!</p>
+        </Modal>
+    </teleport>
+    <br/>
+    <button @click="toggleModal">Open Modal 1</button>
+    <button @click.alt="toggleModal2">Open Modal 2 (Alt + Click)</button>
 </template>
 
 <script>
@@ -13,14 +32,27 @@
         },
         data() {
             return {
-                title: 'My First Vue App :)'
+                title : 'My First Vue App :)',
+                header: 'Sign up for the Giveaway!',
+                text  : 'Grab your ninja swag for half price!',
+                showModal : false,
+                showModal2: false
+            }
+        },
+        methods: {
+            toggleModal() {
+                this.showModal = !this.showModal;
+            },
+
+            toggleModal2() {
+                this.showModal2 = !this.showModal2;
             }
         }
     }
 </script>
 
 <style>
-    #app {
+    #app, #modals {
         font-family: Avenir, Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
